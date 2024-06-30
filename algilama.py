a9af5f74a61ff728174a139ae9708cdc11b123f8
Bilal Efe Doygun <bilaldygn@gmail.com>
2024-07-01 00:44:25 +0300

    Update algilama.py

--- algilama.py
+++ algilama.py
@@ -1,8 +1,22 @@
 import speech_recognition as sr
-r = sr.Recognizer()
+import pyaudio 
+
+# Tanıma nesnesi oluşturma
+recognizer = sr.Recognizer()
+
+# Mikrofonu seçme
 with sr.Microphone() as source:
-    r.adjust_for_ambient_noise(source)
-    data = r.record(source , duration= 5)
-print("tanımlanıyor")
-text = r.recognize_google(data,language='tr')
-print(text)
+  print("Konuşmaya başlayın...")
+  
+  # Ses kaydı alma
+  audio = recognizer.listen(source)
+
+# Konuşmayı tanıma
+try:
+  # Google Web Speech API kullanarak konuşmayı metne dönüştür
+  text = recognizer.recognize_google(audio)
+  print("Söylediğiniz:", text)
+except sr.UnknownValueError:
+  print("Konuşmanızı anlayamadım.")
+except sr.RequestError as e:
+  print("Servis hatası:", e)  
