35e0169031b83f3d3ac6c64f1084f8ab16675dcf
Bilal Efe Doygun <bilaldygn@gmail.com>
2024-07-01 00:40:28 +0300

    Update algilama.py

--- algilama.py
+++ algilama.py
@@ -1,16 +1,8 @@
 import speech_recognition as sr
-
 r = sr.Recognizer()
-
 with sr.Microphone() as source:
-    print("Bir şeyler söyleyin...")
-    audio = r.listen(source)
-
-    try:
-        text = r.recognize_google(audio, language='tr-TR') 
-        print("Söylediğiniz: " + text)
-    except sr.UnknownValueError:
-        print("Ne dediğinizi anlayamadım.")
-    except sr.RequestError as e:
-        print("Ses tanıma servisine ulaşılamıyor; {0}".format(e))
-        
+    r.adjust_for_ambient_noise(source)
+    data = r.record(source , duration= 5)
+print("tanımlanıyor")
+text = r.recognize_google(data,language='tr')
+print(text)
